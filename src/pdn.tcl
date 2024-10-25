9865431fcd9cba8cb405fa39462e75d2dc8612c5
Uri Shaked <uri@urishaked.com>
2024-10-25 10:14:37 +0300

    fix: some metal5 pdn stripes are too short
    Make the PDN denser, to ensure all stripes go full height

--- src/pdn.tcl
+++ src/pdn.tcl
@@ -18,7 +18,7 @@ set_voltage_domain -name {CORE} -power {VPWR} -ground {VGND}
 # standard cell grid
 define_pdn_grid -name {grid} -voltage_domains {CORE} -pins Metal5
 add_pdn_stripe -grid {grid} -layer {Metal1}     -width {0.44}  -pitch {7.56} -offset {0}      -followpins
-add_pdn_stripe -grid {grid} -layer {Metal5}     -width {2.200} -pitch {75.6} -offset {13.600} -extend_to_core_ring
+add_pdn_stripe -grid {grid} -layer {Metal5}     -width {2.200} -pitch {18.9} -offset {13.600} -extend_to_core_ring
 add_pdn_connect -grid {grid} -layers {Metal1 Metal5}
 
 # SRAM grid
